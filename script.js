44a8a2b02161f2c2645cff7d9e434be219b3813d
Marlene Moraes <marlenevmoraes@outlook.com>
2024-02-03 18:55:02 +0000

    dino game

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-const dino = document.querySelector('.dino');
-const background = document.querySelector('.background');
+// Global vars
+const dino = document.querySelector('#dino');
+const background = document.querySelector('#dino-game');
 
 let isJumping = false;
 let isGameOver = false;
-let position = 0;
+let position = 521;
 
+// Press space (keycode 32) to jump function
 function handleKeyUp(event) {
   if (event.keyCode === 32) {
     if (!isJumping) {
@@ -13,31 +15,31 @@ function handleKeyUp(event) {
   }
 }
 
+// Dino's jump
 function jump() {
   isJumping = true;
 
   let upInterval = setInterval(() => {
-    if (position >= 150) {
-      // Descendo
+    if (position >= 620) {
       clearInterval(upInterval);
 
       let downInterval = setInterval(() => {
-        if (position <= 0) {
+        if (position <= 521) {
           clearInterval(downInterval);
           isJumping = false;
         } else {
           position -= 20;
-          dino.style.bottom = position + 'px';
+          dino.style.bottom = `${position}px`;
         }
       }, 20);
     } else {
-      // Subindo
       position += 20;
-      dino.style.bottom = position + 'px';
+      dino.style.bottom = `${position}px`;
     }
   }, 20);
 }
 
+// Appearing cactus
 function createCactus() {
   const cactus = document.createElement('div');
   let cactusPosition = 1000;
@@ -47,21 +49,19 @@ function createCactus() {
 
   cactus.classList.add('cactus');
   background.appendChild(cactus);
-  cactus.style.left = cactusPosition + 'px';
+  cactus.style.left = `${cactusPosition}px`;
 
   let leftTimer = setInterval(() => {
     if (cactusPosition < -60) {
-      // Saiu da tela
       clearInterval(leftTimer);
       background.removeChild(cactus);
     } else if (cactusPosition > 0 && cactusPosition < 60 && position < 60) {
-      // Game over
       clearInterval(leftTimer);
       isGameOver = true;
-      document.body.innerHTML = '<h1 class="game-over">Fim de jogo</h1>';
+      document.main.innerHTML = '<h1 id="game-over">Game Over</h1>';
     } else {
       cactusPosition -= 10;
-      cactus.style.left = cactusPosition + 'px';
+      cactus.style.left = `${cactusPosition}px`;
     }
   }, 20);
 
